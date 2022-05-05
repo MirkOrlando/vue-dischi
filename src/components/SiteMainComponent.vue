87a5fa15afd496c23db6416e3cbe483e8613942e
MirkOrlando <mirko@orlando-m.it>
2022-05-05 15:22:28 +0200

    created call_API

--- src/components/SiteMainComponent.vue
+++ src/components/SiteMainComponent.vue
@@ -3,7 +3,40 @@
 </template>
 
 <script>
+import axios from "axios";
+export default {
+  name: "SiteMainComponent",
+  data() {
+    return {
+      link: "https://flynn.boolean.careers/exercises/api/array/music",
+      records: null,
+      loading: true,
+      error: null,
+    };
+  },
+  methods: {
+    call_API() {
+      axios
+        .get(this.link)
+        .then((response) => {
+          //console.log(this, response);
+          this.records = response.data.response;
+          this.loading = !response.data.success;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.error = error;
+        });
+    },
+  },
+  mounted() {
+    this.call_API();
+  },
+};
 </script>
 
 <style scoped lang="scss">
+main {
+  background-color: $primaryColor;
+}
 </style>
